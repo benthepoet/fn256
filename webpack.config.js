9a0f748589860a94188f8e70337b1108aa3c3e7f
Ben Hanna <benpaulhanna@gmail.com>
2018-07-05 17:26:05 +0000

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const { resolve } = require('path');
+
+const PUBLIC_PATH = resolve(__dirname, 'public');
+const SRC_PATH = resolve(__dirname, 'src');
+
+module.exports = {
+  entry: `${SRC_PATH}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: PUBLIC_PATH
+  },
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, 
+          {
+            loader: 'css-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: PUBLIC_PATH,
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+         target: 'https://ek512.benpaulhanna.com'
+      }
+    }
+  }
+};
