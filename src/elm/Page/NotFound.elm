533963da7c39a7f9aabf96b3b7f13ef6f11f19cd
Ben Hanna <benpaulhanna@gmail.com>
2018-07-06 17:58:53 +0000

    Updated the NotFound page.

--- src/elm/Page/NotFound.elm
+++ src/elm/Page/NotFound.elm
@@ -9,6 +9,17 @@ import Html.Attributes as Attributes
 view =
     Html.div
         [ Attributes.class "columns" ]
-        [ Elements.column
-            [ Html.text "The page could not be found." ]
+        [ Elements.column []
+        , Elements.column
+            [ Html.div 
+                [ Attributes.class "has-text-centered mt-4" ]
+                [ Html.h2
+                    [ Attributes.class "subtitle is-2" ]
+                    [ Html.text "Error" ]
+                , Html.h4 
+                    [ Attributes.class "subtitle is-5" ]
+                    [ Html.text "The page you requested was not found." ]
+                ]
+            ]
+        , Elements.column []
         ]
