9ad29952ab12a4804f0a81b88def4762bc71c917
Ben Hanna <benpaulhanna@gmail.com>
2018-07-06 02:54:43 +0000

    Added layout for Log In page.

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,4 +1,8 @@
+module Main exposing (..)
+
 import Html
+import Html.Attributes as Attributes
+import Html.Events as Events
 import Navigation
 import Route
 import Task
@@ -63,4 +67,93 @@ update msg model =
 
 
 view model =
-    Html.div [] []
+    let
+        elements =
+            case model.route of
+                Route.Protected Route.Home ->
+                    [ Html.div [] [] ]
+                    
+                Route.Public Route.LogIn ->
+                    [ Html.div 
+                        [ Attributes.class "col-3" ] 
+                        []
+                    , Html.div
+                        [ Attributes.class "col-3" ]
+                        [ Html.div
+                            [ Attributes.class "card m-20 p-20" ]
+                            [ Html.h2
+                                [ Attributes.class "text-center" ]
+                                [ Html.small [] [ Html.text "Log in to your account"]
+                                ]
+                            , Html.form
+                                []
+                                [ Html.div 
+                                    [ Attributes.class "form-group" ]
+                                    [ Html.label 
+                                        [ Attributes.class "form-label" ]
+                                        [ Html.text "Email" ]
+                                    , Html.div
+                                        [ Attributes.class "input-group" ]
+                                        [ Html.span
+                                            [ Attributes.class "input-addon" ] 
+                                            [ Html.i
+                                                [ Attributes.class "petalicon petalicon-user" ]
+                                                []
+                                            ]
+                                        , Html.input
+                                            [ Attributes.class "input"
+                                            , Attributes.type_ "email"
+                                            ]
+                                            []
+                                        ]
+                                    ]
+                                , Html.div 
+                                    [ Attributes.class "form-group" ]
+                                    [ Html.label 
+                                        [ Attributes.class "form-label" ]
+                                        [ Html.text "Password" ]
+                                    , Html.div
+                                        [ Attributes.class "input-group" ]
+                                        [ Html.span
+                                            [ Attributes.class "input-addon" ] 
+                                            [ Html.i
+                                                [ Attributes.class "petalicon petalicon-lock-locked" ]
+                                                []
+                                            ]
+                                        , Html.input
+                                            [ Attributes.class "input"
+                                            , Attributes.type_ "password"
+                                            ]
+                                            []
+                                        ]
+                                    ]
+                                , Html.div 
+                                    [ Attributes.class "form-group" ]
+                                    [ Html.button 
+                                        [ Attributes.class "btn full-width"
+                                        , Attributes.type_ "button"
+                                        ]
+                                        [ Html.text "Log In" ]  
+                                    ]
+                                , Html.div
+                                    [ Attributes.class "form-group text-center" ]
+                                    [ Html.text "Don't have an account? "
+                                    , Html.a [] [ Html.text "Sign Up" ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    , Html.div
+                        [ Attributes.class "col-3" ]
+                        []
+                    ]
+                    
+                Route.Public Route.NotFound ->
+                    [ Html.div [] [ Html.text "Not Found" ] ]
+                    
+                Route.Public Route.SignUp ->
+                    [ Html.div [] [] ]
+    in
+        Html.div 
+            [ Attributes.class "row" ] 
+            elements
