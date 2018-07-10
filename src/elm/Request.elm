67d9ab7172a5a0331a9f8d8bcf4938a74ecdf0b7
Ben Hanna <benpaulhanna@gmail.com>
2018-07-10 14:31:40 +0000

    Refactored request construction.

--- src/elm/Request.elm
+++ src/elm/Request.elm
@@ -6,6 +6,14 @@ import Json.Decode as Decode
 import Json.Encode as Encode
 
 
+type alias Document =
+    { name : String
+    , owner : Int
+    , width : Int
+    , height : Int
+    }
+
+
 type alias LoginToken =
     { token : String
     }
@@ -16,8 +24,25 @@ api =
    (++) "/api" 
 
 
+documentDecoder = 
+    Decode.map4 Document
+        (Decode.field "name" Decode.string)
+        (Decode.field "owner" Decode.int)
+        (Decode.field "width" Decode.int)
+        (Decode.field "height" Decode.int)
+
+
 tokenDecoder =
     Decode.map LoginToken <| Decode.field "token" Decode.string
+
+
+documentEncoder document =
+    Encode.object
+        [ ( "name", Encode.string document.name )
+        , ( "owner", Encode.int document.owner )
+        , ( "width", Encode.int document.width )
+        , ( "height", Encode.int document.height )
+        ]
 
 
 loginEncoder email password =
@@ -27,28 +52,66 @@ loginEncoder email password =
         ]
 
 
+createDocument token document =
+    let
+        request = post (api "/documents") token
+    in
+        Http.request
+            { request
+            | body = Http.jsonBody <| documentEncoder document
+            , expect = Http.expectJson documentDecoder
+            }
+
+getDocuments token = 
+    let
+        request = get (api "/documents") token
+    in 
+        Http.request
+            { request | expect = Http.expectJson documentDecoder }
+
+
 login email password =
     let
-        url = api "/auth/login"
-        body = Http.jsonBody <| loginEncoder email password
+        request = post (api "/auth/login") Nothing
     in
-        Http.post url body tokenDecoder
-        
+        Http.request
+            { request
+            | body = Http.jsonBody <| loginEncoder email password
+            , expect = Http.expectJson tokenDecoder
+            }
+
+
 signUp email password =
     let
-        url = api "/auth/signup"
-        body = Http.jsonBody <| loginEncoder email password
+        request = post (api "/auth/signup") Nothing
     in
-        postEmpty url body
+        Http.request
+            { request | body = Http.jsonBody <| loginEncoder email password }
 
 
-postEmpty url body =
-  Http.request
-    { method = "POST"
-    , headers = []
-    , url = url
-    , body = body
-    , expect = Http.expectStringResponse (\_ -> Ok ())
-    , timeout = Nothing
-    , withCredentials = False
-    }
+make method url token =
+    let
+        headers = 
+            case token of
+                Nothing ->
+                    []
+                    
+                Just value ->
+                    [ Http.header "Authorization" <| "Bearer " ++ value ]
+    in
+        { method = method
+        , url = url
+        , headers = headers
+        , body = Http.emptyBody
+        , expect = Http.expectStringResponse (\_ -> Ok ())
+        , timeout = Nothing
+        , withCredentials = False
+        }
+
+
+get =
+    make "GET"
+
+
+post =
+    make "POST"
