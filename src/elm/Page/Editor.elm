41681bcc7cf9ce1c6fabfb60f7c2203d4b3943cb
Ben Hanna <benpaulhanna@gmail.com>
2018-07-17 15:17:01 +0000

    Added base layout for SVG document.

--- src/elm/Page/Editor.elm
+++ src/elm/Page/Editor.elm
@@ -8,6 +8,8 @@ import Html.Attributes as Attributes
 import Html.Events as Events
 import Http
 import Request
+import Svg
+import Svg.Attributes
 
 
 type DocumentState
@@ -60,47 +62,64 @@ view subModel =
                 ]
                 
         Response (Ok document) ->
-            Html.div 
-                [ Attributes.class "flex-1 flex-column" ]
-                [ Html.div
-                    [ Attributes.class "pl shadow-b has-background-link has-text-white" ]
-                    [ Html.span
-                        [ Attributes.class "icon pl-1 pr-1" ]
-                        [ Elements.fas "file-alt" ]
-                    , Html.span
-                        [ Attributes.class "has-text-white has-text-semi-bold" ]
-                        [ Html.text document.name ]
-                    ]
-                , Html.div
-                    [ Attributes.class "columns flex-1 mb-0 mt-0" ]
+            let
+                width = toString document.width
+                height = toString document.height
+            in
+                Html.div 
+                    [ Attributes.class "flex-1 flex-column" ]
                     [ Html.div
-                        [ Attributes.class "column is-narrow pr-0 w-x-small shadow-r has-background-white" ]
-                        [ Elements.columns 
-                            [ Html.div
-                                [ Attributes.class "column ml-0 mr-0 has-text-centered" ]
-                                [ Html.span
-                                    [ Attributes.class "icon tool" ]
-                                    [ Elements.fas "mouse-pointer" ]
-                                , Html.hr 
-                                    [ Attributes.class "tool" ] 
-                                    []
-                                , Html.span
-                                    [ Attributes.class "icon tool" ]
-                                    [ Elements.fas "barcode" ]
-                                , Html.span
-                                    [ Attributes.class "icon tool" ]
-                                    [ Elements.fas "font" ]
-                                , Html.span
-                                    [ Attributes.class "icon tool" ]
-                                    [ Elements.far "square" ]
+                        [ Attributes.class "pl shadow-b has-background-link has-text-white" ]
+                        [ Html.span
+                            [ Attributes.class "icon pl-1 pr-1" ]
+                            [ Elements.fas "file-alt" ]
+                        , Html.span
+                            [ Attributes.class "has-text-white has-text-semi-bold" ]
+                            [ Html.text document.name ]
+                        ]
+                    , Html.div
+                        [ Attributes.class "columns flex-1 mb-0 mt-0" ]
+                        [ Html.div
+                            [ Attributes.class "column is-narrow pr-0 w-x-small shadow-r has-background-white" ]
+                            [ Elements.columns 
+                                [ Html.div
+                                    [ Attributes.class "column ml-0 mr-0 has-text-centered" ]
+                                    [ Html.span
+                                        [ Attributes.class "icon tool" ]
+                                        [ Elements.fas "mouse-pointer" ]
+                                    , Html.hr 
+                                        [ Attributes.class "tool" ] 
+                                        []
+                                    , Html.span
+                                        [ Attributes.class "icon tool" ]
+                                        [ Elements.fas "barcode" ]
+                                    , Html.span
+                                        [ Attributes.class "icon tool" ]
+                                        [ Elements.fas "font" ]
+                                    , Html.span
+                                        [ Attributes.class "icon tool" ]
+                                        [ Elements.far "square" ]
+                                    ]
+                                ]
+                            ]
+                        , Html.div
+                            [ Attributes.class "column has-text-centered overflow-y-scroll" ]
+                            [ Html.div 
+                                [ Attributes.class "mt-1" ]
+                                [ Svg.svg 
+                                    [ Svg.Attributes.viewBox <| "0 0 " ++ width ++ " " ++ height
+                                    , Svg.Attributes.style <| "width: " ++ width ++ "px; height: " ++ height ++ "px;"
+                                    ]
+                                    [ Svg.rect
+                                        [ Svg.Attributes.x "0" 
+                                        , Svg.Attributes.y "0" 
+                                        , Svg.Attributes.width "100%"
+                                        , Svg.Attributes.height "100%"
+                                        , Svg.Attributes.fill "#fff"
+                                        ]
+                                        []
+                                    ]
                                 ]
                             ]
                         ]
-                    , Html.div
-                        [ Attributes.class "column" ]
-                        [ Html.div 
-                            [ Attributes.class "mt-1" ]
-                            []
-                        ]
                     ]
-                ]
