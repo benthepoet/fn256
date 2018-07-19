72e4c4e7372c2929a608d102ce5776229bc4e8ab
Ben Hanna <benpaulhanna@gmail.com>
2018-07-19 19:35:22 +0000

    Added events and messaging for drag and drop.

--- src/elm/Page/Editor.elm
+++ src/elm/Page/Editor.elm
@@ -8,28 +8,40 @@ import Html
 import Html.Attributes as Attributes
 import Html.Events as Events
 import Http
+import Json.Decode as Decode
 import Request.Document
 import Request.Element
 import Svg
 import Svg.Attributes
+import Svg.Events
 import Task
 
 
+type alias DragEvent =
+    { target : Element
+    , dx : Int
+    , dy : Int
+    }
+
+
 type alias Model =
-    { document : Document
+    { dragging : Maybe DragEvent
+    , document : Document
     , elements : List Element
     }
 
 
 type Msg
-    = NoOp
+    = MouseDown Element Int Int
+    | MouseMove Int Int
+    | MouseUp
 
 
 init id user = 
     let
         token = Just user.token
     in
-        Task.map2 Model 
+        Task.map2 (Model Nothing) 
             (Http.toTask <| Request.Document.get token id)
             (Http.toTask <| Request.Element.list token id)
 
@@ -37,9 +49,45 @@ update user msg model =
     let
         token = Maybe.map .token user
     in
-        case msg of
-            NoOp ->
-                ( model, Cmd.none )
+        case Debug.log "editor" msg of
+            MouseDown target x y ->
+                ( { model | dragging = Just <| DragEvent target x y }
+                , Cmd.none
+                )
+                
+            MouseMove x y ->
+                case model.dragging of
+                    Nothing ->
+                        ( model, Cmd.none )
+                    
+                    Just { target, dx, dy } ->
+                        case target of
+                            Element.Circle attributes ->
+                                let
+                                    circle = Element.Circle { attributes 
+                                                    | y = x - dx + attributes.y
+                                                    , y = y - dy + attributes.y
+                                                }
+                                in
+                                    ( { model | dragging = Just <| DragEvent circle x y }
+                                    , Cmd.none
+                                    )
+                                    
+                            Element.Rect attributes ->
+                                let
+                                    rect = Element.Rect { attributes
+                                                | x = x - dx + attributes.x
+                                                , y = y - dy + attributes.y
+                                                }
+                                in
+                                    ( { model | dragging = Just <| DragEvent rect x y }
+                                    , Cmd.none
+                                    )
+                
+            MouseUp ->
+                ( { model | dragging = Nothing }
+                , Cmd.none
+                )
 
 
 viewCanvas =
@@ -60,6 +108,7 @@ viewElement element =
                 [ Svg.Attributes.cx <| toString attributes.x 
                 , Svg.Attributes.cy <| toString attributes.y 
                 , Svg.Attributes.r <| toString attributes.radius
+                , onMouseDown <| MouseDown element
                 ]
                 []
 
@@ -69,6 +118,7 @@ viewElement element =
                 , Svg.Attributes.y <| toString attributes.y 
                 , Svg.Attributes.width <| toString attributes.width
                 , Svg.Attributes.height <| toString attributes.height
+                , onMouseDown <| MouseDown element
                 ]
                 []
 
@@ -122,6 +172,8 @@ view { document, elements } =
                             , Svg.Attributes.width width
                             , Svg.Attributes.height height
                             , Svg.Attributes.viewBox <| String.join " " ["0", "0", width, height]
+                            , onMouseMove MouseMove
+                            , Svg.Events.onMouseUp MouseUp
                             ]
                             (viewCanvas :: List.map viewElement elements)
                         ]
@@ -129,3 +181,15 @@ view { document, elements } =
                 ]
             ]
     
+mousePositionDecoder msg =
+    Decode.map2 msg 
+        (Decode.field "clientX" Decode.int)
+        (Decode.field "clientY" Decode.int)
+        
+        
+onMouseDown msg =
+    Svg.Events.on "mousedown" <| mousePositionDecoder msg
+            
+            
+onMouseMove msg =
+    Svg.Events.on "mousemove" <| mousePositionDecoder msg
