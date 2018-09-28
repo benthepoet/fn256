f38f15472f74070d84763fad56e232f57a9f3d8b
Ben Hanna <benpaulhanna@gmail.com>
2018-09-28 17:08:44 +0000

    Added requirejs build configuration.

---
+++ build.js
@@ -0,0 +1,12 @@
+({
+  appDir: 'public',
+  baseUrl: './',
+  dir: 'dist',
+  mainConfigFile: 'public/main.js',
+  removeCombined: true,
+  skipDirOptimize: true,
+  writeBuildTxt: false,
+  modules: [
+    { name: 'main' }
+  ]
+})
